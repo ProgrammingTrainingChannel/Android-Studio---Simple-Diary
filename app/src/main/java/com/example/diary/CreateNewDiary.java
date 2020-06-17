29c84499d4fd9b7da79a37faaaef68dfe378325d
Miliyon Ayalew <35680940+Miliyonayalew@users.noreply.github.com>
2020-06-17 23:49:34 +0300

    - Android Design files added to both activity and layout -ListView is added -Add,edit,delete diary to database is added

--- app/src/main/java/com/example/diary/CreateNewDiary.java
+++ app/src/main/java/com/example/diary/CreateNewDiary.java
@@ -1,27 +1,139 @@
 package com.example.diary;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.room.Room;
 
+import android.content.Context;
+import android.content.Intent;
+import android.icu.text.CaseMap;
+import android.icu.text.SimpleDateFormat;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.diary.dbhelper.AppDatabase;
+import com.example.diary.entities.DiaryEntity;
+import com.example.diary.utilities.DiaryListAdapter;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateNewDiary extends AppCompatActivity {
+    Context context;
+    EditText diarytitle, edtdiary;
+    TextView createddate;
 
+    LinearLayout linearLayout;
+    String DiaryID,dateTime;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_diary);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        linearLayout = findViewById(R.id.linearLayout);
+
+        createddate = findViewById(R.id.tvwCreatedDate);
+        diarytitle = findViewById(R.id.edttitle);
+        edtdiary = findViewById(R.id.edtdiary);
+
+        String date_n = new SimpleDateFormat("EEE,MMM-dd-yyyy, HH:mm a", Locale.getDefault()).format(new Date());
+        createddate.setText(date_n);
 
 
+        context = this;
+        DiaryID = getIntent().getStringExtra("DiaryID");
+        if (DiaryID != null && !DiaryID.equals("")) {
+            new AgentAsyncTask().execute("Load", DiaryID);
 
+        }
     }
+
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.createnewdiary_menu,menu);
+        inflater.inflate(R.menu.createnewdiary_menu, menu);
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //Handle Item Selection
+        switch (item.getItemId()) {
+            case R.id.app_bar_save:
+                if (diarytitle.getText().toString().trim().equals("") && edtdiary.getText().toString().trim().equals("")) {
+                    Snackbar.make(linearLayout, "Please write you diary.", Snackbar.LENGTH_LONG).show();
+                } else{
+                    if(DiaryID != null && !DiaryID.equals("")){
+                        new AgentAsyncTask().execute("Update", DiaryID, diarytitle.getText().toString(),edtdiary.getText().toString());
+                    }else {
+                        new AgentAsyncTask().execute("Add", diarytitle.getText().toString(), edtdiary.getText().toString());
+                    }
+                    Intent intent = new Intent(context, ListOfDiaryActivity.class);
+                    startActivity(intent);
+                }
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public class AgentAsyncTask extends AsyncTask<String, Void, Integer> {
+
+        DiaryEntity diaryEntity = new DiaryEntity();
+
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        @Override
+        protected Integer doInBackground(String... strings) {
+            AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "DiaryDB").build();
+
+            switch (strings[0]) {
+                case "Add":
+
+                    diaryEntity.CreatedDate = new Date();
+                    diaryEntity.Title = strings[1];
+                    diaryEntity.Content = strings[2];
+
+                    db.diaryDAO().InsertDiary(diaryEntity);
+                    diaryEntity = null;
+                    break;
+                case "Load":
+                    diaryEntity = db.diaryDAO().GetSingleDiary(Integer.parseInt(strings[1]));
+                    break;
+                case "Update":
+                    diaryEntity.Id = Integer.parseInt(strings[1]);
+                    diaryEntity.CreatedDate = new Date();
+                    diaryEntity.Title = strings[2];
+                    diaryEntity.Content = strings[3];
+
+                    db.diaryDAO().UpdateDiary(diaryEntity);
+                    diaryEntity = null;
+                    break;
+            }
+            return 0;
+        }
+
+        @Override
+        protected void onPostExecute(Integer agentsCount) {
+            if (diaryEntity != null) {
+                diarytitle.setText(diaryEntity.Title);
+                edtdiary.setText(diaryEntity.Content);
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/diary/ListOfDiaryActivity.java
+++ app/src/main/java/com/example/diary/ListOfDiaryActivity.java
@@ -2,20 +2,35 @@ package com.example.diary;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import com.example.diary.dbhelper.AppDatabase;
+import com.example.diary.entities.DiaryEntity;
+import com.example.diary.utilities.DiaryListAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.room.Room;
 
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListOfDiaryActivity extends AppCompatActivity {
     public Context context;
+    public AppDatabase db;
+    public ListView lstvwDiaryList;
+    private SearchView searchView;
+    DiaryListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +52,7 @@ public class ListOfDiaryActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        LoadNotes();
     }
 
     @Override
@@ -44,9 +60,44 @@ public class ListOfDiaryActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu,menu);
 
-        return true;
+        searchView = (SearchView) menu.findItem(R.id.app_bar_search)
+                .getActionView();
+
+        searchView.setSubmitButtonEnabled(true);
+
+
+        return super.onCreateOptionsMenu(menu);
+
     }
 
+
+
+    public void LoadNotes(){
+        lstvwDiaryList = (ListView)findViewById(R.id.mydiarysList);
+        db = Room.databaseBuilder(this,AppDatabase.class,"DiaryDB").build();
+
+        new AgentAsyncTask().execute();
+    }
+
+    private class AgentAsyncTask extends AsyncTask<Void, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            List<DiaryEntity> diaryEntities = new ArrayList<>();
+            diaryEntities = db.diaryDAO().GetAll();
+
+            adapter = new DiaryListAdapter(context, diaryEntities);
+
+            lstvwDiaryList.setAdapter(adapter);
+
+            return 0;
+        }
+
+        @Override
+        protected void onPostExecute(Integer agentsCount) {
+
+        }
+    }
 
 
 }
